326f6d307d4cfdcf72e60c78b82271faa31c2b15
jeremys <jeremysurget77@gmail.com>
2019-12-23 23:56:50 +0100

    V1 of search field

--- web.py
+++ web.py
@@ -1,8 +1,14 @@
-from flask import Flask
-from flask import render_template, render_template_string
+from flask import Flask, redirect, url_for
+from flask import render_template, render_template_string, request
 from jinja2 import Template
 
 from pymongo import MongoClient
+
+from elasticsearch import Elasticsearch
+
+ES_LOCAL = True
+
+es_client = Elasticsearch(hosts=["localhost" if ES_LOCAL else "elasticsearch"])
 
 client = MongoClient("0.0.0.0:27018")
 
@@ -28,6 +34,32 @@ def refurbComparaisonPage():
         response.append(document)
 
     return render_template("refurb_page.html", products = response)
+
+
+@app.route('/search/', methods=['GET', 'POST'])
+def searchPage():
+
+    if request.method == 'POST':
+        search_term = request.form["input"]
+        res = es_client.search(
+            index="product", 
+            size=20, 
+            body={
+                "query": {
+                    "multi_match" : {
+                        "query": search_term, 
+                        "fields": [
+                            "title", 
+                            "currentPrice",
+                        ] 
+                    }
+                }
+            }
+        )
+        return render_template('search.html', res=res )
+    #   return redirect(url_for('searchPage'))
+    else:
+      return render_template('search.html')
 
 
 
